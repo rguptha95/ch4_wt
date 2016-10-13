44cebf890907012889c0e7f8728a04e60a2307ca
Revanth Challuru <rchallur@uncc.edu>
2016-10-13 01:15:28 +0000

    Done with section 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,12 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
