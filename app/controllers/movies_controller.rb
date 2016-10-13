7b70a159a740e18caf06591ae5f12af14ca15f77
Revanth Challuru <rchallur@uncc.edu>
2016-10-13 03:45:16 +0000

    Done with section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,13 +5,9 @@ class MoviesController < ApplicationController
   end
 end
 
-def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-end
-
 def show
-    @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
-    # BUG: we should check @movie for validity here!
+  @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
+  # BUG: we should check @movie for validity here!
 end
 
 def new
@@ -19,7 +15,31 @@ def new
 end
 
 def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+  @movie = Movie.create!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
+def edit
+  @movie = Movie.find params[:id]
+end
+
+def update
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+end
+
+def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
+end
+
+private
+
+def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
 end
