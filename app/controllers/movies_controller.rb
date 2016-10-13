bd11909795a1e0d0bb1b5228db2e9b6e0d5f70c2
Revanth Challuru <rchallur@uncc.edu>
2016-10-13 03:35:12 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,3 +13,7 @@ def show
     @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
     # BUG: we should check @movie for validity here!
 end
+
+def new
+  # default: render 'new' template
+end
